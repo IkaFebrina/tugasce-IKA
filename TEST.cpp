c498940f984293ac485f9986411740a62bd50a9f
IkaFebrina <32430894+IkaFebrina@users.noreply.github.com>
2017-10-24 20:51:41 +0800

    Membuat File Praktek0201.cpp

---
+++ TEST.cpp
@@ -0,0 +1,8 @@
+include <iostream>
+using namespace std;
+
+int main(){
+	cout <<"yesterday is history, tommorow is a"<< endl;
+	cout <<"mystery, but today is a gift."<< endl;
+	cout <<"that's why it's called 'PRESENT'"<<;
+}
